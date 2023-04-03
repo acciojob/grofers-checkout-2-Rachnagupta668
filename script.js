353fd96682409db6a9370cca6287eb788caaa6fd
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-04-03 11:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+const priceCells = document.querySelectorAll('[data-ns-test="prices"]');
+  
+  // Initialize total price to 0
+  let totalPrice = 0;
+  
+  // Iterate over price cells and sum up their values
+  priceCells.forEach(cell => {
+    totalPrice += parseFloat(cell.textContent);
+  });
+  
+  // Get grand total cell using data attribute selector
+  const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+  
+  // Set grand total cell's text content to the total price
+  grandTotalCell.textContent = totalPrice;
