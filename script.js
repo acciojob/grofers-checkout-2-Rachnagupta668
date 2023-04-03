34fbd57f6c1d724d622cc38c3efded0c44266d81
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-04-03 11:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
 //your code here
-const priceCells = document.querySelectorAll('[data-ns-test="prices"]');
-  
-  // Initialize total price to 0
-  let totalPrice = 0;
-  
-  // Iterate over price cells and sum up their values
-  priceCells.forEach(cell => {
-    totalPrice += parseFloat(cell.textContent);
-  });
-  
-  // Get grand total cell using data attribute selector
-  const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
-  
-  // Set grand total cell's text content to the total price
-  grandTotalCell.textContent = totalPrice;
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
